c83c025cafd08d957351f070aaa37816f4526cad
alex <alexu.werner@gmail.com>
2024-05-30 19:58:04 +0200

    added pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Charizard', height: '1.7', type: ['fire', 'flying'] },
+  { name: 'Jigglypuff', height: '0.5', type: ['fairy', 'normal'] },
+  { name: 'Houndoom', height: '1.4', type: ['dark', 'fire'] },
+  { name: 'Dewgong', height: '1.7', type: ['ice', 'water'] },
+];
