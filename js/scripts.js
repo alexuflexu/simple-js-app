155e8f4824ec635299cb35931b0a5306ce779b73
alex <alexu.werner@gmail.com>
2024-06-02 18:43:22 +0200

    added inline CSS and loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,36 @@
 let pokemonList = [
-  { name: 'Charizard', height: '1.7', type: ['fire', 'flying'] },
-  { name: 'Jigglypuff', height: '0.5', type: ['fairy', 'normal'] },
-  { name: 'Houndoom', height: '1.4', type: ['dark', 'fire'] },
-  { name: 'Dewgong', height: '1.7', type: ['ice', 'water'] },
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying'],
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal'],
+  },
+  {
+    name: 'Houndoom',
+    height: 1.4,
+    type: ['dark', 'fire'],
+  },
+  {
+    name: 'Dewgong',
+    height: 1.7,
+    type: ['ice', 'water'],
+  },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  // check if the height of the pokemon is below 0.7
+  if (pokemonList[i].height < 0.7) {
+    // if it is below 0.7:
+    // prettier-ignore
+    document.write(pokemonList[i].name +' (height: ' + pokemonList[i].height + ')' + " - Wow! That's small!" + '<br>');
+  } else {
+    // if the height is above 0.7:
+    document.write(
+      pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + '<br>'
+    );
+  }
+}
