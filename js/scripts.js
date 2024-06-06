90f48ffd161dca81692f6bcf55fbaee67fcf6059
alex <alexu.werner@gmail.com>
2024-06-06 21:56:35 +0200

    added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
-let pokemonList = [
-  {
-    name: 'Charizard',
-    height: 1.7,
-    type: ['fire', 'flying'],
-  },
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    type: ['fairy', 'normal'],
-  },
-  {
-    name: 'Houndoom',
-    height: 1.4,
-    type: ['dark', 'fire'],
-  },
-  {
-    name: 'Dewgong',
-    height: 1.7,
-    type: ['ice', 'water'],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Charizard',
+      height: 1.7,
+      type: ['fire', 'flying'],
+    },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      type: ['fairy', 'normal'],
+    },
+    {
+      name: 'Houndoom',
+      height: 1.4,
+      type: ['dark', 'fire'],
+    },
+    {
+      name: 'Dewgong',
+      height: 1.7,
+      type: ['ice', 'water'],
+    },
+  ];
 
-pokemonList.forEach(function (pokemon) {
+  return {
+    getAll: function () {
+      return pokemonList;
+    },
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
   // check if the height of the pokemon is below 0.7
   if (pokemon.height < 0.7) {
     // if it is below 0.7:
